8dfdccfee68385e265733663ba6f18d5cbab894f
ElGualicho <thomasbonnaud093@gmail.com>
2026-05-02 15:17:19 +0200

    Raise inverted player and crystal sprites

--- visual-adjustments.js
+++ visual-adjustments.js
@@ -6,8 +6,8 @@ const PICS_VISUAL_Y_OFFSET = 9;
 const FLAG_VISUAL_Y_OFFSET = 15;
 
 // En gravité inversée, les sprites doivent visuellement coller à la plateforme du dessus.
-const INVERTED_PLAYER_ATTACH_OFFSET = 18;
-const INVERTED_PICS_ATTACH_OFFSET = 17;
+const INVERTED_PLAYER_ATTACH_OFFSET = 21;
+const INVERTED_PICS_ATTACH_OFFSET = 19;
 
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
